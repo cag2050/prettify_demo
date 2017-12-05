be2d35ba377d05828e3a35c293197623ba5de28e
cag2050 <cag2050@163.com>
2017-12-05 16:33:15 +0800

    m

--- src/components/Prettify.vue
+++ src/components/Prettify.vue
@@ -2,12 +2,12 @@
 <div>
     <pre class='prettyprint lang-sql linenums'>
         select * from user
+        select * from user11111
+        select * from user
+        select * from user2222
         select * from user
         select * from user
-        select * from user
-        select * from user
-        select * from user
-        select * from user
+        select * from user33
         select * from user
         select * from user
         select * from user
